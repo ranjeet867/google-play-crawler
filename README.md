ad1148c5e565a33ad5b612db6f6dc2513c80eaf0
Ranjeet Singh <ranjeetsingh867@gmail.com>
2018-02-20 18:20:52 +0530

    Modify to support more reviews

--- README.md
+++ README.md
@@ -1,37 +1,39 @@
 
 # Welcome to the google-play-crawler!
-Python based Google Play Crawler to crawl all the app related data. 
+Python based Google Play Crawler 
 
-##Glimpse of crawl data: 
+##Example of crawled data:
 
-App name:  Amazon Shopping
-Installs Range:   50,000,000 - 100,000,000 
-Rating Value:  4.268221378326416
-Rating Count:  449554
-Reviews Count:  449,554
-Rating:   5 
-Rating count:  281,323
-Rating:   4 
-Rating count:  86,924
-Rating:   3 
-Rating count:  34,203
-Rating:   2 
-Rating count:  14,772
-Rating:   1 
-Rating count:  32,332
-Author Name:  michelle slaughter
-Review Date:  2 June 2016
-Reviewer Link:  /store/apps/details?id=com.amazon.mShop.android.shopping&reviewId=Z3A6QU9xcFRPSEdKRjFBeEJjVFNZMHdfLUptRnprTkhnOGpacWhaSzhUb1NOa29Ca3lYeEtxZi1PeXZZUXVtdVlieExuaG1wbmtSRF83emZLeE1iRXg3dVE
-Reviewer Ratings:  3
-Review Title:  Alright
-Review Body:   Alright The app itself is great. This is my first user on Amazon after being a longtime user of Ebay. Just not too happy with delivery as promised. I ordered 2 instock items directly from Amazon on may 30th, says will ship today (june 2) and have it by the 6th..has yet to be shipped. Funny thing is the next day the 31st I ordered 2 items from a 3rd party on Amazon and they shipped yesterday. Sneaky part tho offering 30 days free of Prime, have to give credit card. I did, mine is expired so they cancelled free trial  Full Review  
-Developer Reply:  
+App name:  Amazon Shopping <br>
+Installs Range:   50,000,000 - 100,000,000
+Rating Value:  4.268221378326416<br>
+Rating Count:  449554<br>
+Reviews Count:  449,554<br>
+Rating:   5 <br>
+Rating count:  281,323<br>
+Rating:   4 <br>
+Rating count:  86,924<br>
+Rating:   3 <br>
+Rating count:  34,203<br>
+Rating:   2 <br>
+Rating count:  14,772<br>
+Rating:   1 <br>
+Rating count:  32,332<br>
+Author Name:  michelle slaughter<br>
+Review Date:  2 June 2016<br>
+Reviewer Link:  /store/apps/details?id=com.amazon.mShop.android.shopping&reviewId=Z3A6QU9xcFRPSEdKRjFBeEJjVFNZMHdfLUptRnprTkhnOGpacWhaSzhUb1NOa29Ca3lYeEtxZi1PeXZZUXVtdVlieExuaG1wbmtSRF83emZLeE1iRXg3dVE<br>
+Reviewer Ratings:  3<br>
+Review Title:  Alright<br>
+Review Body:   Alright The app itself is great. This is my first user on Amazon after being a longtime user of Ebay. Just not too happy with delivery as promised. I ordered 2 instock items directly from Amazon on may 30th, says will ship today (june 2) and have it by the 6th..has yet to be shipped. Funny thing is the next day the 31st I ordered 2 items from a 3rd party on Amazon and they shipped yesterday. Sneaky part tho offering 30 days free of Prime, have to give credit card. I did, mine is expired so they cancelled free trial  Full Review <br> 
+Developer Reply:  <br>
 
 ##Requirements
 
 1. Python 3.4.4
-1. beautifulsoup4
-
+2. beautifulsoup4
+3. Selenium
+4. geckodriver
+5. Firefox
 ## Installation beautifulsoup4
 
 If you're using a recent version of Debian or Ubuntu Linux, you can install Beautiful Soup with the system package manager:
@@ -41,11 +43,21 @@ Try anyone of these methods. I personally prefer pip.
     $ apt-get install python-bs4.
     $ easy_install beautifulsoup4.
     $ pip install beautifulsoup4.
-    $ python setup.py install.
+
+To Install Selenium 
+
+    pip install selenium
+    
+To install geckodriver in Mac
+
+    brew install geckodriver
+    
 
 ## Usage:
-Directly run the script crawl_play_store.py to print app data. By default it will crawl Flipkart and Amazon upto 20 reviews.
+Directly run the script crawl_play_store.py to print app data. By default it will crawl Flipkart and Amazon upto 1000 reviews.
 
+     python crawl_play_store.py
+     
 To scrap data just add app url in this array in python file:
 urls = ["https://play.google.com/store/apps/details?id=com.flipkart.android&hl=en", 
 		"https://play.google.com/store/apps/details?id=com.amazon.mShop.android.shopping"]
--- crawl_play_store.py
+++ crawl_play_store.py
@@ -1,79 +1,88 @@
-import requests
+import time
 from bs4 import BeautifulSoup
-import sys,io
-
+import sys, io
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.proxy import *
 
 # @author Ranjeet Singh <ranjeetsingh867@gmail.com>
-# Moodify it according to your requirements
+# Modify it according to your requirements
+
+no_of_reviews = 1000
 
 non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
+driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
+
+wait = WebDriverWait( driver, 10 )
 
 
-#Append your app store urls here
-urls = ["https://play.google.com/store/apps/details?id=com.flipkart.android&hl=en", 
-		"https://play.google.com/store/apps/details?id=com.amazon.mShop.android.shopping"]
+# Append your app store urls here
+urls = ["https://play.google.com/store/apps/details?id=com.flipkart.android&hl=en",
+        "https://play.google.com/store/apps/details?id=com.amazon.mShop.android.shopping"]
 
 for url in urls:
 
-	page  = requests.get(url).text
-	soup_expatistan = BeautifulSoup(page, "html.parser")
-	
+    driver.get(url)
+    page = driver.page_source
 
-	expatistan_table = soup_expatistan.find("div", class_="id-app-title")
+    soup_expatistan = BeautifulSoup(page, "html.parser")
 
-	print("App name: ", expatistan_table.string)
-	
-	
-	expatistan_table = soup_expatistan.find("div", itemprop="numDownloads")
-	
-	print("Installs Range: ", expatistan_table.string)
+    expatistan_table = soup_expatistan.find("div", class_="id-app-title")
+
+    print("App name: ", expatistan_table.string)
+
+    expatistan_table = soup_expatistan.find("div", itemprop="numDownloads")
+
+    print("Installs Range: ", expatistan_table.string)
+
+    expatistan_table = soup_expatistan.find("meta", itemprop="ratingValue")
+
+    print("Rating Value: ", expatistan_table['content'])
+
+    expatistan_table = soup_expatistan.find("meta", itemprop="ratingCount")
+
+    print("Rating Count: ", expatistan_table['content'])
+
+    expatistan_table = soup_expatistan.find("span", class_="reviews-num")
+
+    print("Reviews Count: ", expatistan_table.string)
+
+    soup_histogram = soup_expatistan.find("div", class_="rating-histogram")
+
+    rating_bars = soup_histogram.find_all('div', class_="rating-bar-container")
+
+    for rating_bar in rating_bars:
+        print("Rating: ", rating_bar.find("span").text)
+        print("Rating count: ", rating_bar.find("span", class_="bar-number").string)
+
+    next_button = driver.find_element_by_xpath('//*[@id="body-content"]/div/div/div[1]/div[2]/div[2]/div[1]/div[4]/button[2]')
+
+    for i in range(0,no_of_reviews):
+        try:
+            next_button.click()
+        except Exception:
+         time.sleep(5)
+
+    reviews_div = driver.find_element_by_xpath('//div[@data-load-more-section-id="reviews"]').get_attribute("innerHTML")
+    soup_expatistan = BeautifulSoup(reviews_div, "html.parser")
+
+    expand_pages = soup_expatistan.find_all("div", class_="single-review")
+
+    for expand_page in expand_pages:
+        print("Author Name: ", str(expand_page.find("span", class_="author-name").string.encode("utf-8")))
+        print("Review Date: ", expand_page.find("span", class_="review-date").string.encode("utf-8"))
+        print("Reviewer Link: ", expand_page.find("a", class_="reviews-permalink")['href'])
+        reviewer_ratings = expand_page.find("div", class_="review-info-star-rating").find_next()['aria-label'];
+        reviewer_ratings = ''.join(x for x in reviewer_ratings if x.isdigit())
+        print("Reviewer Ratings: ", reviewer_ratings)
+        print("Review Title: ", str(expand_page.find("span", class_="review-title").string))
+        print("Review Body: ", str(expand_page.find("div", class_="review-body").text.encode("utf-8")))
+        developer_reply = expand_page.find_parent().find("div", class_="developer-reply")
+        if hasattr(developer_reply, "text"):
+            print("Developer Reply: ", str(developer_reply.text.encode("utf-8")))
+        else:
+            print("Developer Reply: ", "")
 
 
-	expatistan_table = soup_expatistan.find("meta", itemprop="ratingValue")
+driver.quit()
 
-	print("Rating Value: ", expatistan_table['content'])
-
-
-	expatistan_table = soup_expatistan.find("meta", itemprop="ratingCount")
-
-	print("Rating Count: ", expatistan_table['content'])
-
-
-
-	expatistan_table = soup_expatistan.find("span", class_="reviews-num")
-
-	print("Reviews Count: ", expatistan_table.string)
-
-
-	soup_histogram = soup_expatistan.find("div", class_="rating-histogram")
-	
-	rating_bars = soup_histogram.find_all('div', class_ = "rating-bar-container")
-	
-	for rating_bar in rating_bars:
-		print("Rating: ", rating_bar.find("span").text)
-		print("Rating count: ", rating_bar.find("span", class_="bar-number").string)
-		
-	reviews_div = soup_expatistan.find(attrs={"data-load-more-section-id": "reviews"})
-	expand_pages = reviews_div.find_all("div", class_="single-review")
-	
-	for expand_page in expand_pages:
-		print("Author Name: ", str(expand_page.find("span", class_="author-name").a.string).translate(non_bmp_map))
-		print("Review Date: ", expand_page.find("span", class_="review-date").string)
-		print("Reviewer Link: ", expand_page.find("a", class_="reviews-permalink")['href'])
-		reviewer_ratings = expand_page.find("div", class_="review-info-star-rating").find_next()['aria-label'];
-		reviewer_ratings = ''.join(x for x in reviewer_ratings if x.isdigit())
-		print("Reviewer Ratings: ", reviewer_ratings)
-		print("Review Title: ", str(expand_page.find("span", class_="review-title").string).translate(non_bmp_map))
-		print("Review Body: ", str(expand_page.find("div", class_="review-body").text).translate(non_bmp_map))
-		developer_reply = expand_page.find_parent().find("div", class_="developer-reply")
-		if hasattr(developer_reply, "text"):
-			print("Developer Reply: ", str(developer_reply.text).translate(non_bmp_map))
-		else:
-			print("Developer Reply: ", "")
-			
-		
-			
-			
-		
-		
-	
